146a265ed0541e8fb6e2b9eb791d31f8f4b25704
Hugo Jeller <hugo_jeller@hotmail.com>
2019-01-23 06:45:46 -0300

    added api with flask, added git commit monitoring, added sentry

--- ping_checker_cachet_daemon.py
+++ ping_checker_cachet_daemon.py
@@ -5,10 +5,15 @@ import os
 import requests
 import threading
 import functools
+import git
+import sentry_sdk
 from requests.exceptions import ConnectionError, ReadTimeout, ConnectTimeout
 from statistics import stdev
 from dotenv import load_dotenv
 from terminaltables import AsciiTable
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS
 
 print('Loading env file...')
 load_dotenv()
@@ -26,8 +31,10 @@ url = os.getenv('URL')
 metric_id = int(os.getenv('METRIC_ID'))
 acceptable_loss = float(os.getenv('ACCEPTABLE_LOSS'))
 pinging_timeout = float(os.getenv('PINGING_TIMEOUT'))
+sentry_url = os.getenv('SENTRY_URL')
 
 # Static declaration
+current_sha = None
 pop_time = time_to_refresh / ping_history
 api_url = 'http://{0}/PING/{1}'
 servers = []
@@ -35,11 +42,31 @@ headers = {
     'X-Cachet-Token': api_key
 }
 
+sentry_sdk.init(sentry_url)
+app = Flask('PingChecker')
+api = Api(app)
+
+CORS(app)
+
 
 def eprint(*args, **kwargs):
     sys.stdout.flush()
     print(*args, file=sys.stderr, **kwargs)
     sys.stderr.flush()
+
+
+def check_for_new_version():
+    global current_sha
+
+    repo = git.Repo()
+    sha = repo.head.object.hexsha
+
+    if current_sha is None:
+        print('Running on commit #{0}'.format(sha))
+        current_sha = sha
+    elif current_sha != sha:
+        print('New version found, quitting...')
+        quit(0)
 
 
 def cache_dotenv():
@@ -101,6 +128,17 @@ class Server:
         self.avg = -1
         self.jitter = 0
         self.last_pop = 0
+
+    def toJSON(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'url': self.url,
+            'abnormal': self.abnormal(),
+            'ping': self.avg,
+            'baseline': self.minimum(),
+            'jitter': self.stdev(),
+        }
 
     def receive_ping(self, ms: int) -> object:
         self.pings += 1
@@ -166,7 +204,7 @@ class Server:
         self.health_check()
 
         if self.status:
-            ms = ping(sv.url, ip)
+            ms = ping(self.url, ip)
 
             self.receive_ping(ms)
 
@@ -242,55 +280,89 @@ def load_servers():
         servers.append(Server(data))
 
 
-load_servers()
+class ServerApi(Resource):
 
-while True:
-    # Reload DotEnv
-    cache_dotenv()
+    def get(self):
+        svs = list(map(lambda x: x.toJSON(), servers))
 
-    # Dispatch threads
-    for sv in servers:
-        sv.ping()
+        return svs
 
-    # Join them
-    for sv in servers:
-        sv.wait()
 
-    abnormal = 0
+class PingsApi(Resource):
+    def get(self, id):
+        sv = list(filter(lambda x: int(x.id) == int(id), servers))
 
-    # Check for abnormal servers
-    table_data = [['Server URL', 'Average', 'History', 'Loss', 'Abnormal']]
-    for sv in servers:
-        if sv.abnormal():
-            abnormal += 1
-        table_data.append([
-            sv.url,
-            '{0:.2f} +-{1:.2f}'.format(sv.avg, sv.stdev()),
-            len(sv.history),
-            '{0}'.format(sv.loss()),
-            'YES' if sv.abnormal() else '---'
-        ])
+        if len(sv) == 0:
+            return {'error': True}
 
-    table = AsciiTable(table_data)
+        return {
+            'error': False,
+            'pings': sv[0].history,
+        }
 
-    print(table.table)
 
-    # Debug
-    print('Currently {0} abnormal servers.'.format(abnormal))
+def runner():
+    load_servers()
 
-    # Build POST data
-    data = {
-        'value': abnormal,
-        'timestamp': int(time.time()),
-    }
+    while True:
+        check_for_new_version()
 
-    # Send POST
-    try:
-        res = requests.post(url + '/api/v1/metrics/{0}/points'.format(metric_id), data=data, headers=headers)
-        print('Status code for POST: {0}'.format(res.status_code))
-    except:
-        print('Error posting data')
+        # Reload DotEnv
+        cache_dotenv()
 
-    # Flush and wait
-    sys.stdout.flush()
-    time.sleep(interval)
+        # Dispatch threads
+        for sv in servers:
+            sv.ping()
+
+        # Join them
+        for sv in servers:
+            sv.wait()
+
+        abnormal = 0
+
+        # Check for abnormal servers
+        table_data = [['Server URL', 'Average', 'History', 'Loss', 'Abnormal']]
+        for sv in servers:
+            if sv.abnormal():
+                abnormal += 1
+            table_data.append([
+                sv.url,
+                '{0:.2f} +-{1:.2f}'.format(sv.avg, sv.stdev()),
+                len(sv.history),
+                '{0}'.format(sv.loss()),
+                'YES' if sv.abnormal() else '---'
+            ])
+
+        # table = AsciiTable(table_data)
+
+        # print(table.table)
+
+        # Debug
+        print('Currently {0} abnormal servers.'.format(abnormal))
+
+        # Build POST data
+        data = {
+            'value': abnormal,
+            'timestamp': int(time.time()),
+        }
+
+        # Send POST
+        try:
+            res = requests.post(url + '/api/v1/metrics/{0}/points'.format(metric_id), data=data, headers=headers)
+            print('Status code for POST: {0}'.format(res.status_code))
+        except:
+            print('Error posting data')
+
+        print('Sleeping {0} seconds with {1} threads alive'.format(interval, threading.active_count()))
+        # Flush and wait
+        sys.stdout.flush()
+        time.sleep(interval)
+
+
+runner_thread = threading.Thread(target=runner)
+runner_thread.start()
+
+api.add_resource(ServerApi, '/servers/')
+api.add_resource(PingsApi, '/pings/<int:id>')
+
+app.run(debug=True)
